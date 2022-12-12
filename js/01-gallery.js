dc96390974694b6944b7655fcfab362ae8f45a5d
FilBelan <filbelan@gmail.com>
2022-12-12 15:08:00 +0200

    task 7 final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -38,22 +38,66 @@ galleryItems.forEach((item) => {
 
 
 
-let instance = basicLightbox.create(
-  `<img src='${e.target.getAttribute("data-source")}' width="800" height="600">`
-); 
+// let instance = basicLightbox.create(
+//   `<img src='${e.target.getAttribute("data-source")}' width="800" height="600">`
+// ); 
 
-images.addEventListener("click", (e) => {
-  event.preventDefault();
-  if (e.target.nodeName === "IMG") {
+// images.addEventListener("click", (e) => {
+//   event.preventDefault();
+//   if (e.target.nodeName === "IMG") {
    
-    instance.show();
-    console.log(e.target.getAttribute("data-source"));
-  }
-});
+//     instance.show();
+//     console.log(e.target.getAttribute("data-source"));
+//   }
+// });
 
-document.addEventListener("keyup", e => {
-  if (e.key === "Escape") {
-      instance.close()  
-  }
-});
+// document.addEventListener("keyup", e => {
+//   if (e.key === "Escape") {
+//       instance.close()  
+//   }
+// });
 
+
+let instance = null
+ 
+ images.addEventListener("click", (e) => {
+ event.preventDefault();
+ if (e.target.nodeName !== "IMG") {
+ return
+ }
+ instance = basicLightbox.create( 
+ `<img src='${e.target.getAttribute("data-source")}' width="800" height="600">`
+ );
+ 
+ instance.show();
+ console.log(e.target.getAttribute("data-source"));
+ });
+ 
+ document.addEventListener("keyup", e => {
+ if (e.key === "Escape") {
+ instance.close()
+ }
+ }); 
+
+
+
+// galleryEl.addEventListener('click', showBigImage);
+
+// function showBigImage(evt) {
+//   evt.preventDefault()
+//   if (evt.target.nodeName != 'IMG') {
+//     return
+//   }
+//   const instance = basicLightbox.create(`<img src = "${evt.target.dataset.source}" width="800" height="600">`, {
+//     onShow: () => document.addEventListener('keydown', onEscPress),
+//     onClose: () => document.addEventListener('keydown', onEscPress),
+//   })
+
+//   instance.show()
+
+//   function onEscPress(evt) {
+//     if (evt.code === 'Escape') {
+//       instance.close()
+//     }
+//   }
+// }
